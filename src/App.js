db60393bc3bb9300702ceca9551b6664af67a7e8
DariusGalu <darius.galu@bobnet.ro>
2022-07-23 17:56:59 +0300

    routing in progress

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from 'Components/Header/Header.jsx'
 import Home from 'Components/Home';
 import Footer from 'Components/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Restaurante from 'Components/Restaurante';
+// import Restaurante from 'Components/Restaurante';
 function App() {
   return (
     <BrowserRouter>
@@ -11,7 +11,7 @@ function App() {
         <Header />
           <Routes>
             <Route path="/" element={ <Home />}>
-              <Route path="/restaurante" element={<Restaurante />} />
+              {/* <Route path="/restaurante" element={<Restaurante />} /> */}
             </Route>
           </Routes>
         <Footer />
