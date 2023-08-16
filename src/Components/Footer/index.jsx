965c2d9ec141781a5f493decba4b309bf8f9f6ec
Tibi Craciun <tibi@happysoft.ro>
2023-08-16 11:35:35 +0300

    fixed email in contact

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -16,7 +16,7 @@ function Footer() {
     ],
     horecaOrders: [
       { name: t("about"), href: `/${lang}/despre` },
-      { name: t("contact-us"), href: "mailto:denis@horecaorders.com" },
+      { name: t("contact-us"), href: "mailto:" + t("contact-email") },
       { name: t("restaurants-prices"), href: `/${lang}/preturi-restaurante` },
       { name: t("suppliers-prices"), href: `/${lang}/preturi-furnizori` },
     ],
